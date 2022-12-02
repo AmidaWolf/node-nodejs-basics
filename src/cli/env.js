b01ff3fa93c3607a1848ab4e531535c0b255467e
Yauhen Varapayeu <35904041+AmidaWolf@users.noreply.github.com>
2022-12-03 00:47:22 +0300

    feat: add solution for cli section

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,14 @@
 const parseEnv = () => {
-    // Write your code here 
+    const searchString = 'RSS_'
+    let resultString = ''
+
+    Object.keys(process.env).filter((key) => {
+        if (key.includes(searchString)) return key
+    }).forEach((key) => {
+        resultString = resultString + `${key}=${process.env[key]}; `
+    })
+
+    console.log(resultString.slice(0, -2))
 };
 
 parseEnv();
