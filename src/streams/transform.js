b794601336a907fd036128a498b7f79a7fbfaa1a
Yauhen Varapayeu <35904041+AmidaWolf@users.noreply.github.com>
2022-12-04 00:58:25 +0300

    feat: add solution for streams section

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,36 @@
+import * as stream from "stream";
+import readline from "readline";
+
 const transform = async () => {
-    // Write your code here 
-};
+    const reverseString = (str) => [...str].reverse().join('');
+
+    class ReverseStream extends stream.Transform {
+        constructor(options = {}) {
+            super(options);
+        }
+
+        _transform(chunk, encoding, callback) {
+            this.push(reverseString(chunk.toString(), 'utf-8'))
+            callback()
+        }
+    }
+
+    const rl = readline.createInterface({
+        input: process.stdin.pipe(new ReverseStream()),
+        output: process.stdout,
+        prompt: 'Enter a text for take reverse: ',
+    });
+
+    rl.prompt()
+
+    rl.on('line', (line) => {
+        if (line.trim()) {
+            rl.write(line)
+            rl.close();
+        }
+    }).on('close', () => {
+        process.exit(0);
+    })
+}
 
 await transform();
