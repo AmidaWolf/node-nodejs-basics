cbc11fcff17ec9b3a7a5063a53ba8e77f0b604e9
Yauhen Varapayeu <35904041+AmidaWolf@users.noreply.github.com>
2022-12-04 19:47:24 +0300

    feat: add solution for zip section

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,22 @@
+import path from "path";
+import {dirname} from "./constants.js";
+import fs from "fs";
+import * as zlib from "zlib";
+import * as stream from "stream";
+
 const decompress = async () => {
-    // Write your code here 
+    const file = 'fileToCompress.txt'
+    const archive = 'archive.gz'
+    const filePath = path.resolve(dirname, 'files', file);
+    const archivePath = path.resolve(dirname, 'files', archive);
+
+    const readableStream = fs.createReadStream(archivePath)
+    const writableStream = fs.createWriteStream(filePath)
+    const unzip = zlib.createUnzip()
+
+    stream.pipeline(readableStream, unzip, writableStream, (err) => {
+        console.log(err)
+    })
 };
 
 await decompress();
